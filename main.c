1ee772334c6c81f559adf10b4c01771c4f050a4b
KaikyBizon <126878587+KaikyBizon@users.noreply.github.com>
2026-09-15 14:52:08 -0300

    Criação do arquivo principal

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    typedef struct equipamento {
+        int cod_sltc;
+        string cod_equip[7];
+        string nome_equip[21];
+        int prioridade;
+        int periodo;
+    }
+
+
+    int opt;
+    printf("Escolha uma opção: ");
+    scanf("%d", &opt);
+    switch (opt){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 0:
+            break;
+        default:
+            break;
+    }
+}
